eb521c0488d6eb70b22823f8323337e6edb8c95f
Vishrut Jha <vishrutkmr7@icloud.com>
2024-04-26 03:56:04 -0500

    Initial Commit

---
+++ Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Vishrut Jha on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
